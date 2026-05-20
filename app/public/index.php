8775d8f0037f7acf7f2e19473fbb9bc53d446253
Binary-Phantom <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>
2026-05-20 16:12:59 -0300

    fix: index.php

--- app/public/index.php
+++ app/public/index.php
@@ -2,8 +2,16 @@
 
 use App\Kernel;
 
+$_SERVER['APP_ENV'] = $_SERVER['APP_ENV'] ?? 'prod';
+$_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? false;
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-return static function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+return function (array $context) {
+
+    return new Kernel(
+        $context['APP_ENV'],
+        (bool) $context['APP_DEBUG']
+    );
+
 };
